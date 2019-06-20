65ec65229cd39f660a9cde8f6292262812685092
Darthjktu <Darthjktu@gmail.com>
2019-06-20 07:57:35 +0300

    balzap

--- work/balzap/dist/js/common.js
+++ work/balzap/dist/js/common.js
@@ -7,8 +7,25 @@ var Common = {
     main: function() {
         
         
+        $('.catalog__filter-head').on('click',function(){
+            $(this).toggleClass('close').next('.catalog__filter-toggle').slideToggle();
+        })
         
         
+        $('.scroll-pane').jScrollPane({
+			verticalDragMinHeight: 35,
+			verticalDragMaxHeight: 35,
+			horizontalDragMinWidth: 35,
+			horizontalDragMaxWidth: 35
+		});
+        
+        $('select').styler();
+        
+        $('.catalog-left-block').on('click',function(e){
+            e.preventDefault();
+            $(this).toggleClass('open');
+            $('.catalog-left').toggleClass('open');
+        })
     },
     
 };
